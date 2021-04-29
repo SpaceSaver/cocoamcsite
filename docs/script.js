c4b4ab8275ff947c89b4cba38aecd8853262af24
SpaceSaver <aadixonfamily@gmail.com>
2021-04-29 12:19:55 -0400

    Use JavaScript to create and format the packs instead of direct HTML.  Adds a message that shows up if JavaScript is not enabled.

--- docs/script.js
+++ docs/script.js
@@ -5,10 +5,22 @@
         console.log(packs);
         for (var i = 0; i < packs.length; i++) {
             packs[i].outerHTML = "<a href=\"" + packs[i].getAttribute('packdownload') + "\" target=\"_blank\" download>" + packs[i].outerHTML + "</a>"
-            /*packs[i].addEventListener("click", element => {
-                console.log(element.path[element.path.length - 7].getAttribute('packdownload'));
-                window.location.href = element.path[element.path.length - 7].getAttribute('packdownload');
-            });*/
+            var packicon = document.createElement("img");
+            packicon.src = packs[i].getAttribute("data-pack-icon");
+            packicon.classList.add("pack-icon");
+            packs[i].appendChild(packicon);
+            packicon = null;
+            var tdcontainer = document.createElement("div");
+            tdcontainer.classList.add("pack-text-container");
+            var title = document.createElement("p");
+            title.classList.add("pack-title");
+            title.textContent = packs[i].getAttribute("data-pack-title");
+            var description = document.createElement("p");
+            description.classList.add("pack-description");
+            description.textContent = packs[i].getAttribute("data-pack-description");
+            tdcontainer.appendChild(title);
+            tdcontainer.appendChild(description);
+            packs[i].appendChild(tdcontainer);
         }
     }
     window.onload = startup;
